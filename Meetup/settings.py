c90128f319648b472ef813d86902a60e99437b10
Shreyas <dlshreyas30@gmail.com>
2022-08-28 23:00:10 +0530

    Some updates

--- Meetup/settings.py
+++ Meetup/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8#dyrsi8l0v6mnlk5#whc70&ww@24d)kd7hkk_3opujm%akaun'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- meetup_home/views.py
+++ meetup_home/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request) :
-    return render(request,'meetup_home/index.html')
+    meetups = [
+        {'title' : 'A First Meetup'},
+        {'title' : 'A Second Meetup'}
+    ]
+    return render(request,'meetup_home/index.html', {
+        'meetups' : meetups
+    })
